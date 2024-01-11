ae5113312db12ae3dc8b836246b779647bfba0b3
Praveen Kumar <connect.pk@yahoo.com>
2024-01-11 20:52:45 +0530

    added spinner

--- client/src/pages/AddUserToGroup/index.js
+++ client/src/pages/AddUserToGroup/index.js
@@ -6,12 +6,15 @@ export function AddUserToGroup() {
     const userNameRef = useRef('');
     const [groups, setGroups] = useState([]);
     const [selectedGroup, setSelectedGroup] = useState('')
+    const [loading, setLoading] = useState(true);
     const navigate = useNavigate()
     const location = useLocation();
 
     useEffect(() => {
+        setLoading(true)
         axiosInstance.get("/groups").then(res => {
             setGroups(res.data.groups);
+            setLoading(false)
         })
     }, [])
 
@@ -34,22 +37,24 @@ export function AddUserToGroup() {
 
 
     return <div className="container shadow p-5 d-flex justify-content-center align-items-center">
-        <form noValidate className="d-flex flex-column gap-3" onSubmit={handleFormSubmit} >
-            <div className="form-group">
-                <input ref={userNameRef} type="text" placeholder="Username" className="form-control" />
-            </div>
 
-            <div className="form-group">
-                <select class="form-control" value={selectedGroup} onChange={(e) => {
-                    setSelectedGroup(e.target.value)
-                }}>
-                    <option value={''}>Select Group</option>
-                    {groups.map(group =>
-                        <option key={group.slug} value={group.slug}>{group.name}</option>
-                    )}
-                </select>
-            </div>
-            <button type="submit" className="btn btn-outline-primary">Join</button>
-        </form>
+        {loading ? <div className="spinner-border"></div> :
+            <form noValidate className="d-flex flex-column gap-3" onSubmit={handleFormSubmit} >
+                <div className="form-group">
+                    <input ref={userNameRef} type="text" placeholder="Username" className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <select class="form-control" value={selectedGroup} onChange={(e) => {
+                        setSelectedGroup(e.target.value)
+                    }}>
+                        <option value={''}>Select Group</option>
+                        {groups.map(group =>
+                            <option key={group.slug} value={group.slug}>{group.name}</option>
+                        )}
+                    </select>
+                </div>
+                <button type="submit" className="btn btn-outline-primary">Join</button>
+            </form>}
     </div>
 }
